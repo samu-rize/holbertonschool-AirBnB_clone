487c8c23953b07f49071fc57afce9d0ae96d098b
Hakim <doctorwise@kali>
2023-11-14 10:20:58 -0500

    Store first object

---
+++ models/engine/file_storage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+
+class FileStorage:
+    """This is the class FileStorage"""
+
+    __file_path= "file.json"
+    __objects= {}
+
+def all(self):
+    """return the dictionary __objects"""
+    return self.__objects
+
+def new(self, obj):
+    """sets in __objects the obj with key <obj class name>.id"""
+    key = "{}.{}".format(obj.__class__.__name__, obj.id)
+    sel.objects[key] = obj
+
+
+def save(self):
+        """serializes __objects to the JSON file
+        (path: __file_path)"""
+        with open(self.__file_path, 'w') as f:
+            dict = {}
+            for k, v in self.__objects.items():
+                dict[k] = v.to_dict()
+            json.dump(dict, f)
+
+
+ def reload(self):
+        """ deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists
+        otherwise, do nothing. If the file doesn’t exist
+        no exception should be raised)"""
+        from models.base_model import BaseModel
+        try:
+            with open(self.__file_path, 'r') as f:
+                dict = json.load(f)
+                for k, v in dict.items():
+                    v = eval(v["__class__"])(**v)
+                    self.__objects[k] = v
+        except FileNotFoundError:
+            pass
