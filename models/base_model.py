fab4ff6789ac975a29fc066399739bfb6f9784bc
samu-rize <6710@holbertonstudents.com>
2023-11-24 15:47:01 +0100

    add call to method new

--- models/base_model.py
+++ models/base_model.py
@@ -7,14 +7,13 @@ from datetime import datetime
 
 
 
-
 class BaseModel:
     """this is class BaseModel"""
     
     def __init__(self, *args, **kwargs):
         """constructor"""
 
-    from models import storage
+        from models import storage
 
         if kwargs:
             for k, v in kwargs.items():
@@ -26,9 +25,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
-
-    
     def __str__(self):
         """should print: [<class name>] (<self.id>) <self.__dict__>"""
         return "".format(
@@ -38,7 +36,7 @@ class BaseModel:
         """updates the public instance attribute 
         updated_at with the current datetime"""
 
-         from models import storage
+        from models import storage
 
         self.updated_at = datetime.now()
         storage.save()
