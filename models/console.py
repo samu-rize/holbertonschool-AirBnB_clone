1e7e8a3316644c0de4791bedb91e0ae8cdc675d3
samu-rize <6710@holbertonstudents.com>
2023-11-24 15:23:51 +0100

    6. Console 0.0.1

---
+++ models/console.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""A module called console that contains
+the entry point of the command interpreter"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """this is the class"""
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """exit the program"""
+        return True
+
+    def do_EOF(self, line):
+        """exit the program"""
+        return True
+
+    def emptyline(self):
+        """an empty line + ENTER shouldn’t execute anything"""
+        pass
+
+    def help_quit(self):
+        """help quit"""
+        print("Quit command to exit the program\n")
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
