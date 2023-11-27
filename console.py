d82d901f181ddd12510ee9bf380dd50fafd3d41d
hakimbouharb <6869@holbertonstudents.com>
2023-11-27 05:57:08 -0500

    $[Dr.Wise]@(Kali)//:{Holberton}$

--- console.py
+++ console.py
@@ -4,12 +4,17 @@ the entry point of the command interpreter"""
 import cmd
 from models.base_model import BaseModel
 from models import storage
-
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     """this is the class"""
     prompt = "(hbnb) "
-
+    classes= ["BaseModel", "User", "Place", "State", "City", "Amenity", "Review"]
     def do_quit(self, line):
         """exit the program"""
         return True
